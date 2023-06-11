fb11cf45382e656b50411bee646c8a23937533d0
Alex Ronny <alexronny91@gmail.com>
2023-06-10 22:28:47 -0300

    Melhorias, exclusão e edição nos cadastros

--- routes.py
+++ routes.py
@@ -1,67 +1,82 @@
-from flask import render_template, request, flash, Blueprint
+from flask import render_template, request, Blueprint
 from models import Cliente, ProdutoServico, Pedido
 from database import db
 import datetime as dt
 from datetime import datetime
+from markupsafe import Markup
 
 bp = Blueprint('rotas', __name__, template_folder="templates")
+
+@bp.app_template_global()
+def buscarNomeCliente(listaClientes, idCliente):
+    cliente = ''
+    for c in listaClientes:
+        if c.id == idCliente:
+            cliente = c.nomeRazaoSocial
+    return Markup(cliente)
 
 @bp.route('/')
 @bp.route('/index')
 def index():
     return render_template('index.html')
 
+
 @bp.route("/clientes")
 def clientes():
     listaClientes = Cliente.query.all()
     return render_template('clientes.html', clientes=listaClientes)
 
+
 @bp.route("/cadastrar-cliente", methods=['GET', 'POST'])
 def telaCadastroCliente():
     if request.method == 'GET':
         return render_template('cadastrarCliente.html')
-    
-    if request.method == 'POST':
-            nomeRazaoSocial = request.form.get('nome')
-            dataNascimentoAbertura = request.form.get('data')
-            telefone = request.form.get('telefone')
-            email = request.form.get('email')
-            endereco = request.form.get('endereco')
-            cpfCnpj = request.form.get('cpf-cnpj')
-            dataCadastro = dt.date.today()
 
-            if dataNascimentoAbertura is not None and dataNascimentoAbertura != '':
-                converterDataNascimentoAbertura = datetime.strptime(dataNascimentoAbertura, '%Y-%m-%d').date()
-                c = Cliente(nomeRazaoSocial, converterDataNascimentoAbertura, telefone, email, endereco, cpfCnpj, dataCadastro)
-            else:
-                dataNascimentoAbertura = None
-                c = Cliente(nomeRazaoSocial, dataNascimentoAbertura, telefone, email, endereco, cpfCnpj, dataCadastro)
-                
-            
-            
-            db.session.add(c)
-            db.session.commit()
-            return render_template('posCadastro.html', msgSucesso='Cliente cadastrado com sucesso!!!')
+    if request.method == 'POST':
+        nomeRazaoSocial = request.form.get('nome')
+        dataNascimentoAbertura = request.form.get('data')
+        telefone = request.form.get('telefone')
+        email = request.form.get('email')
+        endereco = request.form.get('endereco')
+        cpfCnpj = request.form.get('cpf-cnpj')
+        dataCadastro = dt.date.today()
+
+        if dataNascimentoAbertura is not None and dataNascimentoAbertura != '':
+            converterDataNascimentoAbertura = datetime.strptime(
+                dataNascimentoAbertura, '%Y-%m-%d').date()
+            c = Cliente(nomeRazaoSocial, converterDataNascimentoAbertura,
+                        telefone, email, endereco, cpfCnpj, dataCadastro)
+        else:
+            dataNascimentoAbertura = None
+            c = Cliente(nomeRazaoSocial, dataNascimentoAbertura,
+                        telefone, email, endereco, cpfCnpj, dataCadastro)
+
+        db.session.add(c)
+        db.session.commit()
+        return render_template('posCadastro.html', msgSucesso='Cliente cadastrado com sucesso!!!')
+
 
 @bp.route("/produtos-servicos")
 def produtosServicos():
     listaProdutosServicos = ProdutoServico.query.all()
     return render_template('produtos-servicos.html', produtos_servicos=listaProdutosServicos)
 
+
 @bp.route("/cadastrar-produto-servico", methods=['GET', 'POST'])
 def telaCadastroProdutoServico():
     if request.method == 'GET':
         return render_template('cadastrarProdutoServico.html')
-    
+
     if request.method == 'POST':
         nomeDescricao = request.form.get('nome-descricao')
         valorUnitario = request.form.get('valor-unitario')
 
         i = ProdutoServico(nomeDescricao, valorUnitario)
-        
+
         db.session.add(i)
         db.session.commit()
         return render_template('posCadastro.html', msgSucesso='Produto/Serviço cadastrado com sucesso!!!')
+
 
 @bp.route("/pedidos")
 def pedidos():
@@ -69,30 +84,113 @@ def pedidos():
     listaClientes = Cliente.query.all()
     return render_template('pedidos.html', pedidos=listaPedidos, clientes=listaClientes)
 
+
 @bp.route("/cadastrar-pedido", methods=['GET', 'POST'])
 def telaCadastroPedido():
     listaPedidos = Pedido.query.all()
     listaClientes = Cliente.query.all()
     listaProdutosServicos = ProdutoServico.query.all()
+
+    valorUnitario = 0
+
     if request.method == 'GET':
         return render_template('cadastrarPedido.html', pedidos=listaPedidos, clientes=listaClientes, produtos_servicos=listaProdutosServicos)
-    
+
     if request.method == 'POST':
         cliente = request.form.get('cliente')
         produto = request.form.get('produto-servico')
         quantidade = request.form.get('quantidade')
         dataPedido = dt.date.today()
-        valorProdutoServico = listaProdutosServicos[int(produto)-1].valorUnitario * int(quantidade)
-        valorTotal = valorProdutoServico
+        valorUnitario = 0
+        for p in listaProdutosServicos:
+            if p.id == int(produto):
+                valorUnitario = p.valorUnitario
+        valorTotal = valorUnitario * int(quantidade)
 
-        print(cliente)
-        print(produto)
-        print(quantidade)
-        print(listaProdutosServicos)
+        i = Pedido(dataPedido, cliente, produto, quantidade,
+                   valorUnitario, valorTotal)
 
-        i = Pedido(dataPedido, cliente, produto, quantidade, valorProdutoServico, valorTotal)
-        
         db.session.add(i)
         db.session.commit()
 
         return render_template('posCadastro.html', msgSucesso='Pedido cadastrado com sucesso!!!')
+
+
+@bp.route("/clientes/editar/<int:id>", methods=['PUT'])
+def editarCliente(id):
+    cliente = Cliente.query.get(id)
+
+    if not cliente:
+        return 'Cliente não encontrado', 404
+
+    cliente.nomeRazaoSocial = request.json.get('nomeRazaoSocial')
+    dataNascimentoAbertura = request.json.get('dataNascimentoAbertura')
+    cliente.telefone = request.json.get('telefone')
+    cliente.email = request.json.get('email')
+    cliente.endereco = request.json.get('endereco')
+    cliente.cpfCnpj = request.json.get('cpfCnpj')
+    dataCadastro = request.json.get('dataCadastro')
+
+    if dataNascimentoAbertura is not None and dataNascimentoAbertura != '':
+        converterDataNascimentoAbertura = datetime.strptime(
+            dataNascimentoAbertura, '%Y-%m-%d').date()
+        cliente.dataNascimentoAbertura = converterDataNascimentoAbertura
+    else:
+        dataNascimentoAbertura = None
+        cliente.dataNascimentoAbertura = dataNascimentoAbertura
+    if dataCadastro is not None and dataCadastro != '':
+        converterDataCadastro = datetime.strptime(
+            dataCadastro, '%Y-%m-%d').date()
+        cliente.dataCadastro = converterDataCadastro
+    else:
+        dataCadastro = None
+        cliente.dataCadastro = dataCadastro
+
+    db.session.commit()
+    return 'Cliente atualizado com sucesso!'
+
+@bp.route("/clientes/excluir/<int:id>", methods=['DELETE'])
+def excluirCliente(id):
+    cliente = Cliente.query.get(id)
+    if cliente:
+        pedidosCliente = Pedido.query.filter(Pedido.idCliente == id).all()
+        if (len(pedidosCliente) >= 1):
+            return 'Existe pedido desse cliente, não será possivel excluir.'
+        else:
+            db.session.delete(cliente)
+            db.session.commit()
+            return 'Cliente excluido com sucesso!'
+    else:
+        return 'Cliente não encontrado.', 404
+    
+@bp.route("/produtos-servicos/editar/<int:id>", methods=['PUT'])
+def editarProdutoServico(id):
+    produto = ProdutoServico.query.get(id)
+
+    if not produto:
+        return 'Produto não encontrado', 404
+    
+    nomeDescricao = request.json.get('nomeDescricao')
+    valorUnitario = request.json.get('valorUnitario')
+    
+    if nomeDescricao is not None and nomeDescricao != '' and valorUnitario is not None and valorUnitario != 0: 
+        produto.nomeDescricao = nomeDescricao
+        produto.valorUnitario = valorUnitario
+        db.session.commit()
+        return 'Produto atualizado com sucesso!'
+    else:
+        return 'Todos os campos são obrigatórios!'
+
+@bp.route("/produtos-servicos/excluir/<int:id>", methods=['DELETE'])
+def excluirProdutoServico(id):
+    produtoServico = ProdutoServico.query.get(id)
+    if produtoServico:
+        pedidosComProdutoServico = Pedido.query.filter(Pedido.idProdutoServico == id).all()
+        if (len(pedidosComProdutoServico) >= 1):
+            return 'Existe pedido com esse produto/serviço, não será possivel excluir.'
+        else:
+            db.session.delete(produtoServico)
+            db.session.commit()
+            return 'Produto/Serviço excluido com sucesso.'
+    else:
+        return 'Produto não encontrado.', 404
--- static/scripts.js
+++ static/scripts.js
@@ -1,41 +1,203 @@
 function validarFormulario(nomeFormulario) {
     if (nomeFormulario == 'cliente') {
-        
+
         var nome = document.getElementById('campo-nome').value;
         var telefone = document.getElementById('telefone').value;
         var cpfCnpj = document.getElementById('cpf-cnpj').value;
-    
+
         if (!nome || !telefone || !cpfCnpj) {
             alert('Por favor, preencha todos os campos obrigatórios!');
             return false;
         }
-    
+
         return true;
 
-    } 
-    
+    }
+
     if (nomeFormulario == 'produto-servico') {
         var nomeDescricao = document.getElementById('input-cadastro-nome-descricao').value;
         var precoUnitario = document.getElementById('input-cadastro-valor-unitario').value;
-        
+
         if (!nomeDescricao || !precoUnitario) {
             alert('Por favor, preencha todos os campos!');
             return false;
         }
-    
+
         return true;
     }
-    
+
     if (nomeFormulario == 'pedido') {
         var cliente = document.getElementById('option-cliente').value;
         var produto = document.getElementById('option-produto-servico').value;
         var quantidade = document.getElementById('quantidade-produtos-servicos').value;
-        
+
         if (!cliente || !produto || !quantidade) {
             alert('Por favor, preencha todos os campos!');
             return false;
         }
-    
+
         return true;
     }
 }
+
+$(document).ready(function () {
+
+    $('.excluir-btn').click(function () {
+        var id = $(this).data('id');
+        var textoH2 = $('h2').text();
+
+        if (textoH2 == 'Clientes') {
+            $.ajax({
+                url: '/clientes/excluir/' + id,
+                method: 'DELETE',
+                contentType: 'application/json',
+                success: function (response) {
+                    if (response == 'Existe pedido desse cliente, não será possivel excluir.') {
+                        alert(response)
+                    } else {
+                        var linhaRemover = $("#linha-cliente-" + id);
+                        linhaRemover.remove()
+                        alert(response)
+                    }
+                },
+                error: function (error) {
+                    alert('Erro ao excluir cliente: ' + error.responseText);
+                }
+            });
+        } else if (textoH2 == 'Produtos / Serviços') {
+            $.ajax({
+                url: '/produtos-servicos/excluir/' + id,
+                method: 'DELETE',
+                contentType: 'application/json',
+                success: function (response) {
+                    if (response == 'Existe pedido com esse produto/serviço, não será possivel excluir.') {
+                        alert(response)
+                    } else {
+                        var linhaRemover = $("#linha-produto-" + id);
+                        linhaRemover.remove()
+                        alert(response)
+                    }
+                },
+                error: function (error) {
+                    alert('Erro ao excluir cliente: ' + error.responseText);
+                }
+            });
+        } else {
+            
+        }
+        
+        
+    })
+
+    $('.editar-btn').click(function () {
+        var id = $(this).data('id');
+        var textoH2 = $('h2').text();
+
+        if (textoH2 == 'Clientes') {
+            $('#nomeRazaoSocial-' + id).removeAttr('readonly');
+            $('#dataNascimentoAbertura-' + id).removeAttr('readonly');
+            $('#telefone-' + id).removeAttr('readonly');
+            $('#email-' + id).removeAttr('readonly');
+            $('#endereco-' + id).removeAttr('readonly');
+            $('#cpfCnpj-' + id).removeAttr('readonly');
+            $('#dataCadastro-' + id).removeAttr('readonly');
+            $(this).hide();
+            $('.salvar-btn[data-id="' + id + '"]').show();
+        } else if (textoH2 == 'Produtos / Serviços') {
+            $('#nomeDescricao-' + id).removeAttr('readonly');
+            $('#valorUnitario-' + id).removeAttr('readonly');
+            $(this).hide();
+            $('.salvar-btn[data-id="' + id + '"]').show();
+        } else {
+
+        }
+    });
+
+    $('.salvar-btn').click(function () {
+        var id = $(this).data('id');
+        var textoH2 = $('h2').text();
+
+        if (textoH2 == 'Clientes') {
+            var nomeRazaoSocial = $('#nomeRazaoSocial-' + id).val();
+            var dataNascimentoAbertura = $('#dataNascimentoAbertura-' + id).val();
+            var telefone = $('#telefone-' + id).val();
+            var email = $('#email-' + id).val();
+            var endereco = $('#endereco-' + id).val();
+            var cpfCnpj = $('#cpfCnpj-' + id).val();
+            var dataCadastro = $('#dataCadastro-' + id).val();
+    
+            var data = {
+                nomeRazaoSocial: nomeRazaoSocial,
+                dataNascimentoAbertura: dataNascimentoAbertura,
+                telefone: telefone,
+                email: email,
+                endereco: endereco,
+                cpfCnpj: cpfCnpj,
+                dataCadastro: dataCadastro
+            };
+    
+            var btnSalvar = $(this);
+            var btnEditar = $('.editar-btn[data-id="' + id + '"]');
+    
+            $.ajax({
+                url: '/clientes/editar/' + id,
+                method: 'PUT',
+                contentType: 'application/json',
+                data: JSON.stringify(data),
+                success: function (response) {
+                    alert(response);
+                    $('#nomeRazaoSocial-' + id).attr('readonly', 'readonly');
+                    $('#dataNascimentoAbertura-' + id).attr('readonly', 'readonly');
+                    $('#telefone-' + id).attr('readonly', 'readonly');
+                    $('#email-' + id).attr('readonly', 'readonly');
+                    $('#endereco-' + id).attr('readonly', 'readonly');
+                    $('#cpfCnpj-' + id).attr('readonly', 'readonly');
+                    $('#dataCadastro-' + id).attr('readonly', 'readonly');
+                    btnSalvar.hide();
+                    btnEditar.show();
+                },
+                error: function (error) {
+                    alert('Erro ao atualizar cliente: ' + error.responseText);
+                }
+            });
+        } else if (textoH2 == 'Produtos / Serviços') {
+            var nomeDescricao =  $('#nomeDescricao-' + id).val();
+            var valorUnitario = $('#valorUnitario-' + id).val();
+
+            if (valorUnitario == '') {
+                valorUnitario = 0
+            }
+
+            var data = {
+                nomeDescricao: nomeDescricao,
+                valorUnitario: valorUnitario
+            }
+            
+            var btnSalvar = $(this);
+            var btnEditar = $('.editar-btn[data-id="' + id + '"]');
+
+            $.ajax({
+                url: '/produtos-servicos/editar/' + id,
+                method: 'PUT',
+                contentType: 'application/json',
+                data: JSON.stringify(data),
+                success: function (response) {
+                    if (response == 'Produto atualizado com sucesso!') {
+                        alert(response);
+                        $('#nomeDescricao-' + id).attr('readonly', 'readonly');
+                        $('#valorUnitario-' + id).attr('readonly', 'readonly');
+                        btnSalvar.hide();
+                        btnEditar.show();
+                    } else {
+                        alert(response);
+                    }
+                },
+                error: function (error) {
+                    alert('Erro ao atualizar cliente: ' + error.responseText);
+                }
+            });
+        } else {
+
+        }
+    });
+});
